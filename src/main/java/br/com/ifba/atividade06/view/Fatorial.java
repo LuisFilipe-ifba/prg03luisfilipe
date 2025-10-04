3bf30e6c2654646afea0ba189487d1f53259559f
PC <PC@DESKTOP-3NNTM0F>
2025-10-04 17:25:48 -0300

    Iniciado atividade 06 com classe e jFrame(ainda não funcionais)

---
+++ src/main/java/br/com/ifba/atividade06/view/Fatorial.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package br.com.ifba.atividade06.view;
+
+/**
+ *
+ * @author PC
+ */
+public class Fatorial {
+    
+    public void setValor (int n){
+        
+    }
+    
+    public int getFatorial(){
+        
+    }
+    
+    public String getFormula(){
+        
+    }
+}
